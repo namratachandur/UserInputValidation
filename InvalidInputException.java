f311dd6372aa3b94757a85fede2842f3f43c0ddb
Namrata Chandur <chandur.namratanc@gmail.com>
2025-04-24 19:09:20 -0400

    Update InvalidInputException.java

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,5 +1,6 @@
 public class InvalidInputException extends Exception 
 {
+    // Constructor to initialize the exception with a custom message
     public InvalidInputException(String message) 
     {
         super(message);
